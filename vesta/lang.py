54476dc86d3a5b2a1f8fc134f3fb21cae945d3ae
Tom BUTIN <tom.frefre.13@gmail.com>
2023-05-18 17:19:08 +0200

    Revert changed made to lang.py

--- vesta/lang.py
+++ vesta/lang.py
@@ -1,29 +1,14 @@
 import logging
-from typing import MutableMapping
 
 from .tables import select, Guild
 
 logger = logging.getLogger(__name__)
 
 class Lang:
-    data: dict
 
     def __init__(self, data, session):
         self.data = data
         self.session = session
-
-        def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str = '.') -> MutableMapping:
-            items = []
-            for k, v in d.items():
-                new_key = parent_key + sep + k if parent_key else k
-                if isinstance(v, MutableMapping):
-                    items.extend(flatten_dict(v, new_key, sep=sep).items())
-                else:
-                    items.append((new_key, v))
-            return dict(items)
-
-        for key in self.data.keys():
-            self.data[key] = flatten_dict(self.data[key])
 
     def get(self, item, guild):
         lang = "en"
